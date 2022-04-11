7b3b52437d60b71f8f1a6211d857a60232d4be66
manasmudbari <mdsaddamvoltas@gmail.com>
2022-04-11 21:05:38 +0600

    build upload

---
+++ .next/server/chunks/649.js
@@ -0,0 +1,13 @@
+exports.id = 649;
+exports.ids = [649];
+exports.modules = {
+
+/***/ 9405:
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+window.$ = window.jQuery = __webpack_require__(2947);
+
+/***/ })
+
+};
+;
---
+++ .next/static/chunks/649.8104b13ba34838ae.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[649],{9405:function(n,w,e){window.$=window.jQuery=e(9755)}}]);
