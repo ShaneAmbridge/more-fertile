7167d0737f088fa60c266a1683a71b37e09488d7
manasmudbari <mdsaddamvoltas@gmail.com>
2022-04-11 21:45:15 +0600

    j

--- pages/index.js
+++ pages/index.js
@@ -8,9 +8,17 @@ import Missions from "../components/home/mission/mission";
 import Points from "../components/home/points/points";
 import Services from "../components/home/services/services";
 import OwlSection from "../components/home/caroselSection/owlSection";
+import Head from "next/head";
 export default function Home({ items }) {
 	return (
 		<>
+			<Head>
+				<script
+					src='https://code.jquery.com/jquery-3.6.0.min.js'
+					integrity='sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4='
+					crossOrigin='anonymous'
+				></script>
+			</Head>
 			<Hero />
 			<Container>
 				<div className={styles.disAndRes}>
