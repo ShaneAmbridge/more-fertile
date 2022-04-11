955ed2c9d45209407828e672dda38d39803d4f49
manasmudbari <mdsaddamvoltas@gmail.com>
2022-04-11 21:54:13 +0600

    h

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home({ items }) {
 				src='https://code.jquery.com/jquery-3.6.0.min.js'
 				integrity='sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4='
 				crossOrigin='anonymous'
-			></Script>
+			/>
 
 			<Hero />
 			<Container>
