a7903d60f7ae3c7b95b7def52d043c853cc4de32
manasmudbari <mdsaddamvoltas@gmail.com>
2022-04-12 07:13:35 +0600

    jqury new way

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
+const webpack = require("webpack");
 const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: false,
+	webpack: (config, { dev }) => {
+		config.plugins.push(
+			new webpack.ProvidePlugin({
+				$: "jquery",
+				jQuery: "jquery",
+			})
+		);
+		return config;
+	},
 };
 
 module.exports = nextConfig;
