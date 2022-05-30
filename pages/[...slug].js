d6bf3aed10f59492d1ec3d509ac653b544790b47
manasmudbari <mdsaddamvoltas@gmail.com>
2022-05-30 22:29:04 +0600

    aa

--- pages/[...slug].js
+++ pages/[...slug].js
@@ -9,177 +9,169 @@ import { useRouter } from "next/router";
 import CategorySidebar from "../components/Layout/categorySidebar/categorySidebar";
 
 export default function Home({ data, items }) {
-  const router = useRouter();
+	const router = useRouter();
 
-  const linkPath = router.asPath.split("/");
-  linkPath.shift();
-  linkPath.shift();
+	const linkPath = router.asPath.split("/");
+	linkPath.shift();
+	linkPath.shift();
 
-  const pathArray = linkPath.map((path, i) => {
-    return { breadcrumb: path, href: "/" + linkPath.slice(0, i + 1).join("/") };
-  });
-  const breadcrumb = pathArray;
-  // if (!breadcrumb.includes("category")) {
-  //   breadcrumb.push("category");
-  // }
+	const pathArray = linkPath.map((path, i) => {
+		return { breadcrumb: path, href: "/" + linkPath.slice(0, i + 1).join("/") };
+	});
+	const breadcrumb = pathArray;
+	// if (!breadcrumb.includes("category")) {
+	//   breadcrumb.push("category");
+	// }
 
-  if (!data?.posts?.nodes[0] || !breadcrumb.length)
-    return (
-      <LayoutMain>
-        <div className={styles.notfound}>
-          <div>
-            <h1>We are sorry.</h1>
-            <h1>This post is not avaliable right now.</h1>{" "}
-            <button onClick={() => router.back()}>Go Back</button>
-          </div>
-        </div>
-      </LayoutMain>
-    );
+	if (!data?.posts?.nodes[0] || !breadcrumb.length)
+		return (
+			<LayoutMain>
+				<div className={styles.notfound}>
+					<div>
+						<h1>We are sorry.</h1>
+						<h1>This post is not avaliable right now.</h1>{" "}
+						<button onClick={() => router.back()}>Go Back</button>
+					</div>
+				</div>
+			</LayoutMain>
+		);
 
-  return (
-    <LayoutMain items={items}>
-      <div className={styles.main}>
-        <div className={styles.container}>
-          <p className={styles.breadcrumb}>
-            {breadcrumb && (
-              <>
-                {breadcrumb.map((link, i) => {
-                  return (
-                    <span key={i + "dfdf"}>
-                      {link.breadcrumb}
-                      <>{i !== breadcrumb.length - 1 && <span> &#62; </span>}</>
-                    </span>
-                  );
-                })}
-              </>
-            )}
-          </p>
-          <h5 className={styles.subTitle}>
-            <Link href={breadcrumb[0].href}>
-              <a>{breadcrumb[0].breadcrumb}</a>
-            </Link>
-          </h5>
-          <h1 className={styles.title}>{data?.posts?.nodes[0]?.title}</h1>
+	return (
+		<LayoutMain items={items}>
+			<div className={styles.main}>
+				<div className={styles.container}>
+					<p className={styles.breadcrumb}>
+						{breadcrumb && (
+							<>
+								{breadcrumb.map((link, i) => {
+									return (
+										<span key={i + "dfdf"}>
+											{link.breadcrumb}
+											<>{i !== breadcrumb.length - 1 && <span> &#62; </span>}</>
+										</span>
+									);
+								})}
+							</>
+						)}
+					</p>
+					<h5 className={styles.subTitle}>
+						<Link href={breadcrumb[0].href}>
+							<a>{breadcrumb[0].breadcrumb}</a>
+						</Link>
+					</h5>
+					<h1 className={styles.title}>{data?.posts?.nodes[0]?.title}</h1>
 
-          {data?.posts?.nodes[0]?.featuredImage !== null &&
-            data?.posts?.nodes[0]?.featuredImage && (
-              <div className={styles.heroImageContainer}>
-                <Image
-                  layout="responsive"
-                  width={1024}
-                  height={487}
-                  src={data?.posts?.nodes[0]?.featuredImage?.node.mediaItemUrl}
-                  alt={data?.posts?.nodes[0]?.featuredImage?.node.altText}
-                />
-              </div>
-            )}
+					{data?.posts?.nodes[0]?.featuredImage !== null &&
+						data?.posts?.nodes[0]?.featuredImage && (
+							<div className={styles.heroImageContainer}>
+								<Image
+									layout='responsive'
+									width={1024}
+									height={487}
+									src={data?.posts?.nodes[0]?.featuredImage?.node.mediaItemUrl}
+									alt={data?.posts?.nodes[0]?.featuredImage?.node.altText}
+								/>
+							</div>
+						)}
 
-          <div className={styles.contentandSidebar}>
-            <div
-              dangerouslySetInnerHTML={{
-                __html: data?.posts?.nodes[0]?.content,
-              }}
-              className={styles.content}
-            ></div>
+					<div className={styles.contentandSidebar}>
+						<div
+							dangerouslySetInnerHTML={{
+								__html: data?.posts?.nodes[0]?.content,
+							}}
+							className={styles.content}
+						></div>
 
-            <div className={styles.sidebar}>
-              <CategorySidebar categories={data?.categories} />
-            </div>
-          </div>
-        </div>
-      </div>
-      {/* )} */}
-    </LayoutMain>
-  );
+						<div className={styles.sidebar}>
+							<CategorySidebar categories={data?.categories} />
+						</div>
+					</div>
+				</div>
+			</div>
+			{/* )} */}
+		</LayoutMain>
+	);
 }
 export async function getStaticPaths() {
-  return {
-    paths: [{ params: { slug: ["male hot", "eggs and age"] } }],
-    fallback: "blocking", // false or 'blocking'
-  };
+	return {
+		paths: [{ params: { slug: ["male hot", "eggs and age"] } }],
+		fallback: "blocking", // false or 'blocking'
+	};
 }
 export async function getStaticProps({ params }) {
-  // console.log(params, "params");
-  const name = params.slug[params.slug.length - 1];
-  const categoryName = params?.slug[1];
+	// console.log(params, "params");
+	const name = params.slug[params.slug.length - 1];
+	const categoryName = params?.slug[1];
 
-  const { data } = await client.query({
-    query: gql`
-      query post($name: String, $slug: [String]) {
-        posts(where: { name: $name }) {
-          nodes {
-            content
-            title
-            uri
-            featuredImage {
-              node {
-                altText
-                mediaItemUrl
-              }
-            }
-          }
-        }
+	const { data } = await client.query({
+		query: gql`
+			query post($name: String, $slug: [String]) {
+				posts(where: { name: $name }) {
+					nodes {
+						content
+						title
+						uri
+						featuredImage {
+							node {
+								altText
+								mediaItemUrl
+							}
+						}
+					}
+				}
 
-        categories(where: { slug: $slug }) {
-          nodes {
-            name
-            uri
-            posts {
-              nodes {
-                title
-                uri
-                slug
-              }
-            }
-            children {
-              nodes {
-                name
-                uri
-                slug
-                posts {
-                  nodes {
-                    title
-                    uri
-                    slug
-                  }
-                }
-                children {
-                  nodes {
-                    name
-                    uri
-                    slug
-                    posts {
-                      nodes {
-                        title
-                        uri
-                        slug
-                      }
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    `,
-    variables: {
-      name: name,
-      slug: categoryName,
-    },
-  });
+				categories(where: { slug: $slug }) {
+					nodes {
+						name
+						uri
+						posts {
+							nodes {
+								title
+								uri
+								slug
+							}
+						}
+						children {
+							nodes {
+								name
+								uri
+								slug
+								posts {
+									nodes {
+										title
+										uri
+										slug
+									}
+								}
+								children {
+									nodes {
+										name
+										uri
+										slug
+										posts {
+											nodes {
+												title
+												uri
+												slug
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		`,
+		variables: {
+			name: name,
+			slug: categoryName,
+		},
+	});
 
-<<<<<<< HEAD
 	// console.log(postOrTree, "postOrTree");
 	return {
 		props: { data },
-		revalidate: 5,
+		revalidate: 10,
 	};
-=======
-  // console.log(postOrTree, "postOrTree");
-  return {
-    props: { data },
-    revalidate: 10,
-  };
->>>>>>> c3bb86aa1aec2d271916da5784bca632dfe5bdfc
 }
