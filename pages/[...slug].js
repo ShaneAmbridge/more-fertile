be5006db14003d8a89246ce2a33014c5b08333f3
md sahadat hossin <mdsahadathossin778@gmail.com>
2022-05-31 16:21:55 +0600

    added sigle post page styleing

--- pages/[...slug].js
+++ pages/[...slug].js
@@ -1,4 +1,5 @@
 import LayoutMain from "../components/Layout/layout";
+import React, { useEffect } from "react";
 import styles from "../styles/post.module.scss";
 import { gql } from "@apollo/client";
 import client from "../apollo-client";
@@ -35,6 +36,55 @@ export default function Home({ data, items }) {
         </div>
       </LayoutMain>
     );
+
+  useEffect(() => {
+    const headTitleName = document.querySelector(
+      ".responsive-table__head__title--name"
+    );
+    const headTitleStatus = document.querySelector(
+      ".responsive-table__head__title--status"
+    );
+    const headTitleTypes = document.querySelector(
+      ".responsive-table__head__title--types"
+    );
+    const headTitleUpdate = document.querySelector(
+      ".responsive-table__head__title--update"
+    );
+    const headTitleCountry = document.querySelector(
+      ".responsive-table__head__title--country"
+    );
+
+    // Select tbody text from Dom
+    const bodyTextName = document.querySelectorAll(
+      ".responsive-table__body__text--name"
+    );
+    const bodyTextStatus = document.querySelectorAll(
+      ".responsive-table__body__text--status"
+    );
+    const bodyTextTypes = document.querySelectorAll(
+      ".responsive-table__body__text--types"
+    );
+    const bodyTextUpdate = document.querySelectorAll(
+      ".responsive-table__body__text--update"
+    );
+    const bodyTextCountry = document.querySelectorAll(
+      ".responsive-table__body__text--country"
+    );
+
+    // Select all tbody table row from Dom
+    const totalTableBodyRow = document.querySelectorAll(
+      ".responsive-table__body .responsive-table__row"
+    );
+
+    // Get thead titles and append those into tbody table data items as a "data-title" attribute
+    for (let i = 0; i < totalTableBodyRow.length; i++) {
+      bodyTextName[i].setAttribute("data-title", headTitleName.innerText);
+      bodyTextStatus[i].setAttribute("data-title", headTitleStatus.innerText);
+      bodyTextTypes[i].setAttribute("data-title", headTitleTypes.innerText);
+      bodyTextUpdate[i].setAttribute("data-title", headTitleUpdate.innerText);
+      bodyTextCountry[i].setAttribute("data-title", headTitleCountry.innerText);
+    }
+  }, []);
 
   return (
     <LayoutMain items={items}>
@@ -172,6 +222,6 @@ export async function getStaticProps({ params }) {
   // console.log(postOrTree, "postOrTree");
   return {
     props: { data },
-    revalidate: 1,
+    revalidate: 2,
   };
 }
