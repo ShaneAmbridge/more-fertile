44b415f1e7ceb77c84334308699af0a04df3fd17
md sahadat hossin <mdsahadathossin778@gmail.com>
2022-06-14 12:50:38 +0600

    added link on bigmenu navbar

--- components/Layout/resourcesBigMenu/resourcesBigMenu.js
+++ components/Layout/resourcesBigMenu/resourcesBigMenu.js
@@ -13,43 +13,62 @@ const ResourcesBigMenu = () => {
               </Link>
             </h4>
             <div className={styles.container}>
-              <div className={styles.item}>
-                <Image
-                  src={"/images/dropdown/male-health.svg"}
-                  width={20}
-                  height={20}
-                  alt=""
-                />
-                <span>Male Health</span>
-              </div>
+              <Link href="/categories/category/fertility-health/health/male-health">
+                <a>
+                  <div className={styles.item}>
+                    <Image
+                      src={"/images/dropdown/male-health.svg"}
+                      width={20}
+                      height={20}
+                      alt=""
+                    />
 
-              <div className={styles.item}>
-                <Image
-                  src={"/images/dropdown/female-health.svg"}
-                  width={14.5}
-                  height={23.2}
-                  alt=""
-                />
-                <span>Female Health</span>
-              </div>
-              <div className={styles.item}>
-                <Image
-                  src={"/images/dropdown/glossary-group.svg"}
-                  width={20}
-                  height={22}
-                  alt=""
-                />
-                <span>Glossary</span>
-              </div>
-              <div className={styles.item}>
-                <Image
-                  src={"/images/dropdown/fetility-tests.svg"}
-                  width={15}
-                  height={22.5}
-                  alt=""
-                />
-                <span>Fetility Tests</span>
-              </div>
+                    <span>Male Health</span>
+                  </div>
+                </a>
+              </Link>
+              <Link href="/categories/category/fertility-health/health/female-health">
+                <a>
+                  <div className={styles.item}>
+                    <Image
+                      src={"/images/dropdown/female-health.svg"}
+                      width={14.5}
+                      height={23.2}
+                      alt=""
+                    />
+
+                    <span>Female Health</span>
+                  </div>
+                </a>
+              </Link>
+              <Link href="/fertility-health/glossary">
+                <a>
+                  <div className={styles.item}>
+                    <Image
+                      src={"/images/dropdown/glossary-group.svg"}
+                      width={20}
+                      height={22}
+                      alt=""
+                    />
+
+                    <span>Glossary</span>
+                  </div>
+                </a>
+              </Link>
+              <Link href="/fertility-health/fertility-tests">
+                <a>
+                  <div className={styles.item}>
+                    <Image
+                      src={"/images/dropdown/fetility-tests.svg"}
+                      width={15}
+                      height={22.5}
+                      alt=""
+                    />
+
+                    <span>Fetility Tests</span>
+                  </div>
+                </a>
+              </Link>
             </div>
           </div>
           <div className={styles.items}>
@@ -59,34 +78,48 @@ const ResourcesBigMenu = () => {
               </Link>
             </h4>
             <div className={styles.container}>
-              <div className={styles.item}>
-                <Image
-                  src={"/images/dropdown/male-conditions.svg"}
-                  width={20}
-                  height={20}
-                  alt=""
-                />
-                <span>Female Conditions</span>
-              </div>
+              <Link href="/categories/category/fertility-conditions/conditions/male-conditions">
+                <a>
+                  <div className={styles.item}>
+                    <Image
+                      src={"/images/dropdown/male-conditions.svg"}
+                      width={20}
+                      height={20}
+                      alt=""
+                    />
 
-              <div className={styles.item}>
-                <Image
-                  src={"/images/dropdown/female-conditions.svg"}
-                  width={14.5}
-                  height={23.2}
-                  alt=""
-                />
-                <span>Female Conditions</span>
-              </div>
-              <div className={styles.item}>
-                <Image
-                  src={"/images/dropdown/both-sexes.svg"}
-                  width={14}
-                  height={23}
-                  alt=""
-                />
-                <span>Both Sexes</span>
-              </div>
+                    <span>Male Conditions</span>
+                  </div>
+                </a>
+              </Link>
+              <Link href="/categories/category/fertility-conditions/female/female-conditions">
+                <a>
+                  <div className={styles.item}>
+                    <Image
+                      src={"/images/dropdown/female-conditions.svg"}
+                      width={14.5}
+                      height={23.2}
+                      alt=""
+                    />
+
+                    <span>Female Conditions</span>
+                  </div>
+                </a>
+              </Link>
+              <Link href="/categories/category/fertility-conditions/both-sexes/unexplained-infertility">
+                <a>
+                  <div className={styles.item}>
+                    <Image
+                      src={"/images/dropdown/both-sexes.svg"}
+                      width={14}
+                      height={23}
+                      alt=""
+                    />
+
+                    <span>Unexplained</span>
+                  </div>
+                </a>
+              </Link>
             </div>
           </div>
           <div className={styles.items}>
@@ -96,48 +129,81 @@ const ResourcesBigMenu = () => {
               </Link>
             </h4>
             <div className={styles.container}>
-              <div className={styles.item}>
-                <Image
-                  src={"/images/dropdown/male-conditions.svg"}
-                  width={20}
-                  height={20}
-                  alt=""
-                />
-                <span>Male Treatments</span>
-              </div>
+              <Link href="/categories/category/fertility-treatments/male-treatments">
+                <a>
+                  <div className={styles.item}>
+                    <Image
+                      src={"/images/dropdown/male-conditions.svg"}
+                      width={20}
+                      height={20}
+                      alt=""
+                    />
 
-              <div className={styles.item}>
-                <Image
-                  src={"/images/dropdown/female-treatments.svg"}
-                  width={14.5}
-                  height={23.2}
-                  alt=""
-                />
-                <span>Female Treatments</span>
-              </div>
-              <div className={styles.item}>
-                <Image
-                  src={"/images/dropdown/both-sexes.svg"}
-                  width={14}
-                  height={23}
-                  alt=""
-                />
-                <span>Both Sexes</span>
-              </div>
+                    <span>Male Treatments</span>
+                  </div>
+                </a>
+              </Link>
+              <Link href="/categories/category/fertility-treatments/female-treatments">
+                <a>
+                  <div className={styles.item}>
+                    <Image
+                      src={"/images/dropdown/female-treatments.svg"}
+                      width={14.5}
+                      height={23.2}
+                      alt=""
+                    />
+                    <span>Female Treatments</span>
+                  </div>
+                </a>
+              </Link>
+              <Link href="/categories/category/fertility-treatments/female-treatments/ivf">
+                <a>
+                  <div className={styles.item}>
+                    <Image
+                      src={"/images/dropdown/both-sexes.svg"}
+                      width={14}
+                      height={23}
+                      alt=""
+                    />
+                    <span>IVF Protocols</span>
+                  </div>
+                </a>
+              </Link>
             </div>
           </div>
           <div className={styles.items}>
-            <h4>Lifestyle & Fertility</h4>
+            <Link href="/categories/category/fertility-health/lifestyle-fertility">
+              <a>
+                <h4>Lifestyle & Fertility</h4>
+              </a>
+            </Link>
             <div className={styles.container}>
-              <div className={styles.item}>
-                <Image
-                  src={"/images/dropdown/age-fertility.svg"}
-                  width={20}
-                  height={20}
-                  alt=""
-                />
-                <span>Age & Fertility</span>
-              </div>
+              <Link href="/categories/category/fertility-health/lifestyle-fertility/lifestyle-factors">
+                <a>
+                  <div className={styles.item}>
+                    <Image
+                      src={"/images/dropdown/age-fertility.svg"}
+                      width={20}
+                      height={20}
+                      alt=""
+                    />
+                    <span>Lifestyle Factors</span>
+                  </div>
+                </a>
+              </Link>
+              <Link href="/categories/category/fertility-health/lifestyle-fertility/lifestyle-substances">
+                <a>
+                  <div className={styles.item}>
+                    <Image
+                      src={"/images/dropdown/age-fertility.svg"}
+                      width={20}
+                      height={20}
+                      alt=""
+                    />
+                    <span>Lifestyle Substances</span>
+                  </div>
+                </a>
+              </Link>
             </div>
           </div>
         </div>
